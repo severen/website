469f14b855a2bf96ddd5d22a6ba48f89dd550c0e
Severen Redwood <me@severen.dev>
2022-11-02 00:24:09 +1300

    Please Prettier

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -7,8 +7,5 @@ const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 
 module.exports = {
-  plugins: [
-    tailwindcss(),
-    autoprefixer,
-  ],
+  plugins: [tailwindcss(), autoprefixer],
 };
