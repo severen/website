87b2d2219ad3e242dfe80896a380b2e8318b4bb0
Severen Redwood <me@severen.dev>
2023-10-22 11:46:09 +1300

    Update dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,21 +5,33 @@
 
 module.exports = {
   root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:svelte/recommended",
+    "plugin:svelte/prettier",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
 
   parser: "@typescript-eslint/parser",
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-  plugins: ["svelte3", "@typescript-eslint"],
-
-  ignorePatterns: ["*.cjs"],
-  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
-
-  settings: {
-    "svelte3/typescript": () => require("typescript"),
-  },
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2020,
+    extraFileExtensions: [".svelte"],
   },
+
+  ignorePatterns: ["*.cjs"],
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
+
   env: {
     browser: true,
     es2017: true,
