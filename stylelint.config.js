ab0ca2c51e2ce0d9759a28b7f9b016a141e1a913
Severen Redwood <sev@severen.dev>
2025-02-19 22:08:08 +1300

    Set up CSS linting with Stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2025 Severen Redwood <sev@severen.dev>
+// SPDX-License-Identifier: CC0-1.0
+
+// @ts-check
+
+/** @type {import('stylelint').Config} */
+export default {
+  extends: "stylelint-config-standard",
+  customSyntax: "postcss-html",
+};
