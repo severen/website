85802547347adb4bd63ce3303b5eb1580a269c92
Severen Redwood <sev@severen.dev>
2025-02-19 21:53:21 +1300

    Configure Vite to transform and minify CSS with Lightning CSS
    Install Lightning CSS and configure Vite to use it for for transforming
    and minifying CSS. The main benefit of this is that any CSS syntax
    features which are not baseline (available for the past two years in
    major browsers) are lowered to baseline syntax.

--- astro.config.ts
+++ astro.config.ts
@@ -3,5 +3,13 @@
 
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      transformer: "lightningcss",
+    },
+    build: {
+      cssMinify: "lightningcss",
+    },
+  },
+});
